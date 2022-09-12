a7fa9012a281ed096795a870cf18c2c0240a2940
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-12 11:52:02 +0300

    Initial Commit

---
+++ ColorPanel/ColorPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPanelApp.swift
+//  ColorPanel
+//
+//  Created by John Doe on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorPanel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorPanel
+//
+//  Created by John Doe on 12/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
